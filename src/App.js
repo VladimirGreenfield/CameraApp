913d5d33a6ca68e4f182bfcdf36b08a01af92252
vladimir greenfield <vladdygreenfield@gmail.com>
2022-01-07 15:38:01 -0500

    created takePhoto function and assigned it to a button, now I am able to capture an image from the web cam stream

--- src/App.js
+++ src/App.js
@@ -18,6 +18,21 @@ function App() {
     })
   }
 
+  const takePhoto = () => {
+    const width = 414;
+    const height = width / (16/9);
+
+    let video = videoRef.current;
+    let photo = photoRef.current;
+
+    photo.width = width;
+    photo.height = height;
+
+    let ctx = photo.getContext('2d');
+    ctx.drawImage(video, 0, 0, width, height);
+    setHasPhoto(true);
+  }
+
   useEffect(() => {
     getVideo();
   }, [videoRef])
@@ -27,7 +42,7 @@ function App() {
 
       <div className="camera">
         <video ref={videoRef}></video>
-        <button>SNAP</button>
+        <button onClick={takePhoto}>SNAP</button>
       </div>
 
       <div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
