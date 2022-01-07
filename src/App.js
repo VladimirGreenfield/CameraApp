1bb0d78f47bca199f6a88c86314ae072de69eb73
vladimir greenfield <vladdygreenfield@gmail.com>
2022-01-07 14:57:19 -0500

    created video ref with a snap button and a canvas ref with close button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { useRef, useEffect, useState} from "react";
 
 function App() {
+  const videoRef = useRef(null);
+  const photoRef = useRef(null);
+  
   return (
     <div className="App">
 
       <div className="camera">
         <video ref={videoRef}></video>
         <button>SNAP</button>
+      </div>
+
+      <div className="result">
+        <canvas ref={photoRef}></canvas>
+        <button>CLOSE</button>
       </div>
 
     </div>
