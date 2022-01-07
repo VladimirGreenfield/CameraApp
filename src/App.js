b09f2cde5b85cfb8c82c23f030acde716cb79820
vladimir greenfield <vladdygreenfield@gmail.com>
2022-01-07 15:04:46 -0500

    created getVideo function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import React, { useRef, useEffect, useState} from "react";
 function App() {
   const videoRef = useRef(null);
   const photoRef = useRef(null);
+
+  const [hasPhoto, setHasPhoto] = useState(false);
+
+  const getVideo = () => {
+    navigator.mediaDevices.getUserMedia({video: {width: 1920, height: 1080}})
+    .then(stream => {
+      let video = videoRef.current;
+      video.srcObject = stream;
+      video.play();
+    })
+  }
   
   return (
     <div className="App">
@@ -12,7 +23,7 @@ function App() {
         <button>SNAP</button>
       </div>
 
-      <div className="result">
+      <div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
         <canvas ref={photoRef}></canvas>
         <button>CLOSE</button>
       </div>
